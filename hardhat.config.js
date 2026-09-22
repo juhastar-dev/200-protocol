f0c8d9fe8170804aee8ef679bd28d2c5f331f238
juhastar-dev <juhastar@gmail.com>
2026-09-22 20:11:09 +0300

    Add locked Base Mainnet network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,14 @@ if (process.env.BASE_SEPOLIA_RPC_URL && process.env.BASE_SEPOLIA_DEPLOYER_PRIVAT
   };
 }
 
+if (process.env.BASE_MAINNET_RPC_URL && process.env.BASE_MAINNET_DEPLOYER_PRIVATE_KEY) {
+  networks.baseMainnet = {
+    url: process.env.BASE_MAINNET_RPC_URL,
+    chainId: 8453,
+    accounts: [process.env.BASE_MAINNET_DEPLOYER_PRIVATE_KEY]
+  };
+}
+
 module.exports = {
   solidity: {
     version: "0.8.24",
